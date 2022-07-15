cb88b2882ab62a3aaea5b41ca219bf95b172fc2b
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-07-15 15:46:02 -0700

    first test written

---
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Vendor name", "Vendor description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
