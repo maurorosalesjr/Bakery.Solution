78165059042211eaddd912a36bec9cdc7247069e
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-07-15 19:31:10 -0700

    first orders test passed, second written

--- Bakery.Tests/ModelTests/OrdersTests.cs
+++ Bakery.Tests/ModelTests/OrdersTests.cs
@@ -13,8 +13,22 @@ namespace Bakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Orders()
     {
-      Orders newOrder = new Orders("Title", "Description", 1, "Date")
+      Orders newOrder = new Orders("Title", "Description", 1, "Date");
       Assert.AreEqual(typeof(Orders), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      //Arrange
+      string title = "croissants"
+      Orders newOrder = new Orders(title, "Description", 1, "Date");
+
+      //Act
+      string result = newOrder.Title;
+
+      //Assert
+      Assert.AreEqual(title, result)
     }
   }
 }
